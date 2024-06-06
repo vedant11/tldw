ec8f2d82f282ce63e5f81968be90b98409d0a88f
Vedant Panchal <pvipulkumar@me.iitr.ac.in>
2024-06-06 22:22:38 +0530

    add args for prompts

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 add yt video id and use local llm to summarize the transcription attached with the video
 
 credits: https://github.com/jdepoix/youtube-transcript-api
+
+## setup
+
+```sh
+pip install -r requirements.txt
+ollama serve
+```
+
+## usage
+
+```sh
+python3 main.py --help
+```
--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-import litellm
+import argparse
 import os
 
-from youtube_transcript_api import YouTubeTranscriptApi
+import litellm
 from dotenv import load_dotenv
+
+from youtube_transcript_api import YouTubeTranscriptApi
 
 load_dotenv()  # Load environment variables from .env file
 
-video_id = ''
+parser=argparse.ArgumentParser()
+parser.add_argument("-v","--vid", help="Add video ID", required=True)
+parser.add_argument("-s","--summary_type", help="Summary type freetext, e.g. short/medium/long/technical/SFW/etc.", default="succinct")
+args=vars(parser.parse_args())
+
+# args
+video_id = args['vid']
+summary_desc = args['summary_type']
+
+# assert
 assert video_id != '', 'Please provide a video_id'
+
+# Get summary
 raw_transcript = list(YouTubeTranscriptApi.get_transcript(video_id))
 trancript = ""
 for t in raw_transcript:
@@ -19,10 +32,11 @@ with open('transcript.txt', 'w') as f:
 res = litellm.completion(
     model=os.getenv('MODEL', 'ollama/llama3'),
     messages=[
-        {"role":"system","content":"help the user to summarize this transcript"},
+        {"role":"system","content":f"help the user to summarize this transcript. Summary should be according to what user needs: {summary_desc}"},
         {"role":"user","content":trancript}
     ]
 )
 
+# Save summary
 with open('summary.txt', 'w') as f:
     f.write(res.choices[0].message['content'])
